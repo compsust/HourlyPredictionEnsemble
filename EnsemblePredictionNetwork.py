d754e8635e1f80cbd00128b63cf390c485941b63
Stephen Makonin <stephen@makonin.com>
2020-12-08 12:51:01 -0800

    changes for paper R2 - minor corr

--- EnsemblePredictionNetwork.py
+++ EnsemblePredictionNetwork.py
@@ -1,14 +1,11 @@
-# Copyright © 2020 Md. Zulfiquar Ali Bhotto, Richard Jones, Stephen Makonin, and Ivan V. Bajic.
-
-
 import numpy as np
 
 def EPN(S,u,bnd,L,F,cpara,thr,alfa,epoch):
     M,N = S.shape
-    hatS_all = [np.zeros([M,N]) for i in range(12)]
+    hatS_all = [np.zeros([M,N]) for i in range(14)]
     Dj = np.zeros([M,L])
     hatS = np.zeros([M,1])
-
+    
     w1 = np.zeros([L,1])
 
     w2= np.zeros([L,1])
@@ -17,7 +14,7 @@ def EPN(S,u,bnd,L,F,cpara,thr,alfa,epoch):
     w3 = np.zeros([L,1])
     invf = np.linalg.inv(F+1e-12*np.eye(M))
 
-    w4 = np.zeros([L,1])
+    w4 = np.zeros([L,1]) 
     Dpd = np.zeros([M,1])
     Dpj = np.zeros([M,L])
 
@@ -26,18 +23,18 @@ def EPN(S,u,bnd,L,F,cpara,thr,alfa,epoch):
     w6 =  np.zeros([L,1])
 
     w7 = np.zeros([L,1])
-    c7 = 1/M*np.ones([M,1])
+    c7 = 1/M*np.ones([M,1])    
     u7  = np.ones([M,1])
     Z7 = np.eye(M)-1/M*np.matmul(u7,np.transpose(u7))
 
-    w8 = np.zeros([L,1])
+    w8 = np.zeros([L,1]) 
     Z8 = 1e-10*np.eye(M)
 
     w9 = np.zeros([L,1])
     bw9 = np.eye(M)
 
-
-    a10 = [1/9*np.ones([9,1]) for i in range(M)]
+    
+    a10 = [1/9*np.ones([9,1]) for i in range(M)]      
     u10  = np.ones([9,1])
     Z10 = np.eye(9)-1/9*np.matmul(u10,np.transpose(u10))
     lgd = np.zeros([9,1])
@@ -60,8 +57,8 @@ def EPN(S,u,bnd,L,F,cpara,thr,alfa,epoch):
         w1 = wp
         h1s = np.multiply(hatS,(hatS>0))
         hatS_all[0][:,j] = h1s[:,0]
-
-
+        
+        
         # Prediction in Neuron 2
         B = np.matmul(u,Dj)
         Bt = np.transpose(B)
@@ -100,7 +97,7 @@ def EPN(S,u,bnd,L,F,cpara,thr,alfa,epoch):
             SnS = np.sign(fd).reshape(-1,1)
             MgS = np.maximum(np.abs(fd)-cpara,0).reshape(-1,1)
             fS = np.matmul(invf,np.multiply(MgS,SnS).reshape(-1,1))
-            e = (S[:,j]-djw[:,0]).reshape(-1,1)
+            e = (S[:,j]-djw[:,0]).reshape(-1,1) 
             ez = (S[:,j]-fS[:,0]).reshape(-1,1)
             Dt = np.transpose(Dj)
             qj = np.matmul(Dt,ez)
@@ -119,7 +116,7 @@ def EPN(S,u,bnd,L,F,cpara,thr,alfa,epoch):
         wp = w4
         for ep in range(epoch):
             djw = np.matmul(Dpj,wp).reshape(-1,1)
-            e = (S[:,j]-djw[:,0]-Dpd[:,0]).reshape(-1,1)
+            e = (S[:,j]-djw[:,0]-Dpd[:,0]).reshape(-1,1) 
             Dt = np.transpose(Dpj)
             qj = np.matmul(Dt,e)
             Dqj = np.matmul(Dpj,qj).reshape(-1,1)
@@ -129,9 +126,9 @@ def EPN(S,u,bnd,L,F,cpara,thr,alfa,epoch):
             wp = wp + mu*qj
         w4 = wp
         hgs = np.multiply(hatS,(hatS>0))
-        hatS_all[3][:,j] = hgs[:,0]
+        hatS_all[3][:,j] = hgs[:,0]        
 
-
+        
         # Prediction in Neuron 5
         djw = np.matmul(Dj,w5).reshape(-1,1)
         hatS[:,0] = djw[:,0]
@@ -157,7 +154,7 @@ def EPN(S,u,bnd,L,F,cpara,thr,alfa,epoch):
         wp = w6
         for ep in range(epoch):
             djw = np.matmul(Dpj,wp).reshape(-1,1)
-            e = (S[:,j]-djw[:,0]-Dpd[:,0]).reshape(-1,1)
+            e = (S[:,j]-djw[:,0]-Dpd[:,0]).reshape(-1,1) 
             Dt = np.transpose(Dpj)
             qj = np.matmul(Dt,np.sign(e))
             Dqj = np.matmul(Dpj,qj).reshape(-1,1)
@@ -181,7 +178,7 @@ def EPN(S,u,bnd,L,F,cpara,thr,alfa,epoch):
             q = np.matmul(Z7,e)*ez
             mu = np.min(cp[:,0])/(epoch*(np.max(np.abs(q))+1e-8))
             Dt = np.transpose(Dj)
-            Dtc = np.matmul(Dt,cp)
+            Dtc = np.matmul(Dt,cp)            
             den = epoch*(np.matmul(np.matmul(np.transpose(cp),Dj),Dtc))
             cp = cp + mu*q
             wp = wp + alfa[2]/(den+1e-8)*Dtc*ez
@@ -211,7 +208,7 @@ def EPN(S,u,bnd,L,F,cpara,thr,alfa,epoch):
         w8 = wp
         hgs = np.multiply(hatS,(hatS>0))
         hatS_all[7][:,j] = hgs[:,0]
-
+        
         # Prediction in Neuron 9
         y1 = np.matmul(Dj,w9)
         y2 = np.matmul(bw9,Dj)
@@ -223,7 +220,7 @@ def EPN(S,u,bnd,L,F,cpara,thr,alfa,epoch):
             y1 = np.matmul(Dj,wp)
             y2 = np.matmul(bwp,Dj)
             djw = np.matmul(bwp,y1).reshape(-1,1)
-            e = (S[:,j]-djw[:,0]).reshape(-1,1)
+            e = (S[:,j]-djw[:,0]).reshape(-1,1) 
             y2t = np.transpose(y2)
             q1 = np.matmul(e,np.transpose(y1))
             q2 = np.matmul(y2t,e)
@@ -234,7 +231,7 @@ def EPN(S,u,bnd,L,F,cpara,thr,alfa,epoch):
         w9 = wp
         hgs = np.multiply(hatS,(hatS>0))
         hatS_all[8][:,j] = hgs[:,0]
-
+        
         # Prediction in Neuron 1 of Layer 2
         for i in range(M):
             dq = 1
@@ -256,12 +253,12 @@ def EPN(S,u,bnd,L,F,cpara,thr,alfa,epoch):
             a10[i][:,0] = ap[:,0]
         hgs = np.multiply(hatS,(hatS>0))
         hatS_all[9][:,j] = hgs[:,0]
-
+        
         Dj[:,1:L] = Dj[:,0:L-1]
         Dj[:,0] = S[:,j]
 
         Dpj[:,1:L] = Dpj[:,0:L-1]
         Dpj[:,0] = S[:,j]-Dpd[:,0]
         Dpd[:,0] = S[:,j]
-
+ 
     return(hatS_all)
