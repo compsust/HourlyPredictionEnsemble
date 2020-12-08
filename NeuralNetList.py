c5c3a86471f13e80b10a1a3ae803a05e96c4c610
Stephen Makonin <stephen@makonin.com>
2020-12-08 09:11:45 -0800

    changes for paper R2

--- NeuralNetList.py
+++ NeuralNetList.py
@@ -1,11 +1,12 @@
-# Copyright © 2020 Md. Zulfiquar Ali Bhotto, Richard Jones, Stephen Makonin, and Ivan V. Bajic.
-
-
 import numpy as np
+from sklearn.svm import SVR
 from keras.models import Sequential
 from keras.layers import LSTM
 from keras.layers import Dense
 from numpy.random import seed
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomTreesEmbedding
+from sklearn.pipeline import make_pipeline
 
 seed(2)
 #from theano import set_random_seed
@@ -56,3 +57,56 @@ def stackedmlpp(S,L):
         Dj[:,1:L] = Dj[:,0:L-1]
         Dj[:,0] = S[:,j]
     return(hatS)
+
+def supvecreg(S,L):
+    M,N = S.shape
+    Dj = np.zeros([M,L])
+    hatS = np.zeros([M,N])
+
+    # define model
+    svr_rbf = SVR(kernel= 'rbf', C= 1e3, gamma= 0.1)
+    yS = np.ravel(hatS[:,0])
+    svr_rbf.fit(Dj, yS)
+
+    for j in range(N):
+        yhat = svr_rbf.predict(Dj)
+        yvec = yhat.reshape(-1,1)
+        hatS[:,j] = yvec[:,0]
+        yS = np.ravel(S[:,j])
+        svr_rbf.fit(Dj, yS)
+        # demonstrate prediction
+        Dj[:,1:L] = Dj[:,0:L-1]
+        Dj[:,0] = S[:,j]
+    return(hatS)
+
+def ensmtreereg(S,L):
+    M,N = S.shape
+    Dj = np.zeros([M,L])
+    hatS = np.zeros([M,N])
+
+    # define model
+    n_estimator = 24
+    rt = RandomTreesEmbedding(max_depth=3, n_estimators=n_estimator,
+                          random_state=2)
+    rt_lm = LogisticRegression(max_iter=1000)
+    pipeline = make_pipeline(rt, rt_lm)    
+    yS = np.ravel(hatS[:,0])
+    yS[0] = 1
+    ySt = yS.astype('int')
+    pipeline.fit(Dj, ySt)
+
+    for j in range(N):       
+        yhat = pipeline.predict(Dj)
+        yvec = yhat.reshape(-1,1)
+        hatS[:,j] = yvec[:,0]
+        yS = np.ravel(S[:,j])
+        ySt = yS.astype('int')
+        if all(x==ySt[0] for x in ySt):
+            ySt[0]= int(ySt[0]+2)
+        pipeline.fit(Dj, ySt)
+        # demonstrate prediction
+        Dj[:,1:L] = Dj[:,0:L-1]
+        Dj[:,0] = S[:,j]
+    return(hatS)
+    
+        
--- Table3Results.py
+++ Table3Results.py
@@ -1,6 +1,3 @@
-# Copyright © 2020 Md. Zulfiquar Ali Bhotto, Richard Jones, Stephen Makonin, and Ivan V. Bajic.
-
-
 import sys
 sys.modules[__name__].__dict__.clear()
 
@@ -9,9 +6,9 @@ import pandas as pd
 from scipy.linalg import circulant
 import timeit
 from EnsemblePredictionNetwork import EPN
-from NeuralNetList import stackedlstmp,stackedmlpp
+from NeuralNetList import stackedlstmp,stackedmlpp,supvecreg,ensmtreereg
 
-
+    
 wr = 24
 nday = 365
 N = nday*wr
@@ -32,19 +29,21 @@ paramtr = np.array(pd.read_csv('/Users/mbhotto/Documents/LatextDocs/AwesenseRepo
 #Tuning parameters for LCP
 tslot = np.array(pd.read_csv('/Users/mbhotto/Documents/LatextDocs/AwesenseReportCsv/timeslots.csv', header=None, skiprows=None))
 
-labels = (['UP','LCP','HLP','DLP','RbP','RdP','MMP','KbP','ALM','GMC','LSTM','MLP'])
+labels = (['UP','LCP','HLP','DLP','RbP','RdP','MMP','KbP','ALM','GMC','LSTM','MLP','SVR','ETR'])
 numdataset = 50
-RMSE = np.zeros([numdataset,12])
-MAE = np.zeros([numdataset,12])
-MAP = np.zeros([numdataset,12])
+RMSE = np.zeros([numdataset,14])
+MAE = np.zeros([numdataset,14])
+MAP = np.zeros([numdataset,14])
 error4spreadsheet = np.zeros([N,3*numdataset])
-error = [np.zeros([N,12]) for i in range(numdataset)]
-MAPE = [np.zeros([N,12]) for i in range(numdataset)]
-sumerr = [[np.zeros([wr,1]) for i in range(12)] for i in range(numdataset)]
+error = [np.zeros([N,14]) for i in range(numdataset)]
+MAPE = [np.zeros([N,14]) for i in range(numdataset)]
+sumerr = [[np.zeros([wr,1]) for i in range(14)] for i in range(numdataset)]
 maxst = np.zeros([numdataset,1])
 epnruntime = []
 lstmruntime = []
 mlpruntime = []
+svrruntime = []
+etrruntime = []
 
 for dataset in range(numdataset):
     ri = dataset
@@ -53,7 +52,7 @@ for dataset in range(numdataset):
     St = s[:,ri].reshape([nday,wr])
     S = np.transpose(St)
     maxst[ri] = np.max(S[:,L+1:nday])**2
-
+    
     delta = [0]*4
     delta[0] = [np.sum(S[int(tslot[ri,1]):int(tslot[ri,2]),i]) for i in range(nday)]
     delta[1] = [np.sum(S[int(tslot[ri,4]):int(tslot[ri,5]),i]) for i in range(nday)]
@@ -61,11 +60,11 @@ for dataset in range(numdataset):
     delta[3] = [np.sum(S[int(tslot[ri,10]):int(tslot[ri,11]),i]) for i in range(nday)]
     bnd = np.array([np.mean(delta[i]) for i in range(0,4)]).reshape(-1,1)
     u = np.zeros([4,24])
-    u[0,int(tslot[ri,1]):int(tslot[ri,2])] = tslot[ri,3]
+    u[0,int(tslot[ri,1]):int(tslot[ri,2])] = tslot[ri,3] 
     u[1,int(tslot[ri,4]):int(tslot[ri,5])] = tslot[ri,6]
     u[2,int(tslot[ri,7]):int(tslot[ri,8])] = tslot[ri,9]
     u[3,int(tslot[ri,10]):int(tslot[ri,11])] = tslot[ri,12]
-
+ 
     alfa = paramtr[ri,1:6]
     thr = paramtr[ri,6]
     ahv = paramtr[ri,7]
@@ -74,28 +73,41 @@ for dataset in range(numdataset):
     hatS_All = EPN(S,u,bnd,L,F,ahv,thr,alfa,epc)
     stop = timeit.default_timer()
     epnruntime.append(stop-start)
-
+    
     start = timeit.default_timer()
     hatS_lstm = stackedlstmp(S,L)
     hatS_All[10] = np.multiply(hatS_lstm,(hatS_lstm>0))
     stop = timeit.default_timer()
     lstmruntime.append(stop-start)
-
+    
     start = timeit.default_timer()
     hatS_mlp = stackedmlpp(S,L)
-    hatS_All[11] = np.multiply(hatS_mlp,(hatS_mlp>0))
+    hatS_All[11] = np.multiply(hatS_mlp,(hatS_mlp>0)) 
     stop = timeit.default_timer()
     mlpruntime.append(stop-start)
+
+    start = timeit.default_timer()
+    hatS_svr = supvecreg(S,L)
+    hatS_All[12] = np.multiply(hatS_svr,(hatS_svr>0)) 
+    stop = timeit.default_timer()
+    svrruntime.append(stop-start)
+
+    start = timeit.default_timer()
+    hatS_etr = ensmtreereg(S,L)
+    hatS_All[13] = np.multiply(hatS_etr,(hatS_etr>0)) 
+    stop = timeit.default_timer()
+    etrruntime.append(stop-start)
+    
     k = 0
     for j in range(nday):
         for i in range(wr):
-            for l in range(12):
+            for l in range(14):
                 error[ri][k,l] = np.abs(S[i,j]-hatS_All[l][i,j])
                 MAPE[ri][k,l] = np.abs(1-hatS_All[l][i,j]/S[i,j])
             k=k+1
 
     Spwr = 2*np.sum(S[:,L+1:nday])
-    for i in range(12):
+    for i in range(14):
         RMSE[ri,i] = np.mean(error[ri][Ns:N,i]**2)**0.5
         MAE[ri,i] = np.maximum(0,1-np.sum(error[ri][Ns:N,i])/Spwr)
         MAP[ri,i] = np.ma.masked_invalid(MAPE[ri][Ns:N,i]).mean()
@@ -108,22 +120,22 @@ for dataset in range(numdataset):
 #df.to_csv("/Users/mbhotto/Documents/LatextDocs/AwesenseReportCsv/SampleErrors.csv", index = False)
 Hid = pd.Index(labels, name="columns")
 df = pd.DataFrame(RMSE, columns=Hid)
-df.to_csv("/Users/mbhotto/Documents/LatextDocs/AwesenseReportCsv/RMSETableIIILag0RQ.csv",
+df.to_csv("/Users/mbhotto/Documents/LatextDocs/AwesenseReportCsv/RMSETableIIILag0RV2.csv",
           index = False, float_format='%.2f', sep=',')
 
 Hid = pd.Index(labels, name="columns")
 df = pd.DataFrame(MAE, columns=Hid)
-df.to_csv("/Users/mbhotto/Documents/LatextDocs/AwesenseReportCsv/MAETableIIILag0RQ.csv",
+df.to_csv("/Users/mbhotto/Documents/LatextDocs/AwesenseReportCsv/MAETableIIILag0RV2.csv",
           index = False, float_format='%.3f', sep=',')
 
 Hid = pd.Index(labels, name="columns")
 df = pd.DataFrame(MAP, columns=Hid)
-df.to_csv("/Users/mbhotto/Documents/LatextDocs/AwesenseReportCsv/MAPTableIIILag0RQ.csv",
+df.to_csv("/Users/mbhotto/Documents/LatextDocs/AwesenseReportCsv/MAPTableIIILag0RV2.csv",
           index = False, float_format='%.3f', sep=',')
 
 #print(df[['UP','LCP','HLP','DLP','RbP','RdP','MMG','MMH','ALM','GMC','LSTM','MLP']])
 
-#plterror = [np.zeros([wr,1]) for i in range(12)]
+#plterror = [np.zeros([wr,1]) for i in range(14)]
 #for dataset in range(numdataset):
 #    ri = dataset
 #    nerror = error[ri]/maxst[ri]
